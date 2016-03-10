4a394b0373424554e32e5bf4c53fa57375dd3fe6
jrupp <jrupp@rightpoint.com>
2016-03-09 21:58:49 -0600

    Progress, but having issues syncing up with the robot every time we start up

--- server/index.js
+++ server/index.js
@@ -12,6 +12,16 @@ io.on('connection', function(socket) {
     console.log('socket.io got connection');
   socket.on('drive', function (data) {
     console.log('drive', data);
+    var drive = Math.floor(data.drive * 500);
+    var turn = data.turn === 0 ? 32768 : -Math.floor(data.turn * 2000);
+    if(data.drive == 0 && data.turn) {
+        // spin in place - set speed based on how far turn is set
+        drive = Math.floor(Math.abs(data.turn) * 500);
+        turn = data.turn > 0 ? -1 : 1; 
+    }
+    var send = new Buffer([ 137, drive >> 8, drive, turn >> 8, turn ]);
+    console.log(send);
+    serialClient.write(send);
   });
   socket.on('mode', function (data) {
     console.log('mode', data);
@@ -34,29 +44,40 @@ serialClient.on('error', function(err) {
     console.log('err serial', err);
 });
 serialClient.connect(serialPort, host, function () {
-    console.log('connected serial');
-    // serialClient.write(new Buffer([128]));
-    // console.log('wrote start');
-    // setTimeout(function() {
-    //     serialClient.write(new Buffer([131]));
-    //     console.log('wrote safe');
-    //     setTimeout(function() {
-    //         serialClient.write(new Buffer([137, 1, 0, 0, 0]));
-    //         console.log('wrote drive');
-    //         setTimeout(function() {
-    //             serialClient.write(new Buffer([137, 0, 0, 0, 0]));
-    //             console.log('wrote no drive');
-    //             setTimeout(function() {
-    //                 serialClient.write(new Buffer([173]));
-    //                 console.log('wrote stop');
-                    
-    //             }, 500);
-    //         }, 500);
-    //     }, 100);
-    // }, 100);
+    console.log('connected serial, waiting for BRC wakup to finish');
+    setTimeout(function() {
+        // 16 nulls to clear out any pending commands,then a reset
+        serialClient.write(new Buffer([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7]));
+        console.log('wrote reset');
+        setTimeout(function() {
+            serialClient.write(new Buffer([128]));
+            console.log('wrote start');
+            setTimeout(function() {
+                serialClient.write(new Buffer([149, 35]));
+                console.log('requested status');
+                setTimeout(function() {
+                    serialClient.write(new Buffer([131]));
+                    console.log('wrote safe');
+                //     setTimeout(function() {
+                //         serialClient.write(new Buffer([137, 1, 0, 0, 0]));
+                //         console.log('wrote drive');
+                //         setTimeout(function() {
+                //             serialClient.write(new Buffer([137, 0, 0, 0, 0]));
+                //             console.log('wrote no drive');
+                //             // setTimeout(function() {
+                //             //     serialClient.write(new Buffer([173]));
+                //             //     console.log('wrote stop');
+                //             //     
+                //             // }, 500);
+                //         }, 500);
+                //     }, 100);
+                }, 100);
+            }, 2000);
+        }, 10000);
+    }, 2000);
 });
 serialClient.on('data', function (data) {
-    console.log('got data: ' + data);
+    console.log('got data: ', new Buffer(data));
 });
 serialClient.on('close', function () {
     console.log('disconnected serial');
@@ -69,8 +90,12 @@ controlClient.on('error', function(err) {
 });
 controlClient.connect(controlPort, host, function () {
     console.log('connected cmd');
-    controlClient.write(new Buffer([0,0,1,0]));
-    console.log('wrote');
+    controlClient.write(new Buffer([0,0,1,0,2,0]));
+    console.log('wrote startup');
+    setTimeout(function() {
+        controlClient.write(new Buffer([2,1]));
+        console.log('pulled BRC back up');
+    }, 1000);
 });
 controlClient.on('data', function (data) {
     console.log('got data cmd: ' + data);
--- web/src/home/homeController.js
+++ web/src/home/homeController.js
@@ -6,19 +6,25 @@
         self.drive = 0;
         self.turn = 0;
 
-        function applyDeadzone(value) {
+        function applyDeadzone(value, oldValue) {
             if (!value) {
                 return value;
             }
             var dz = 0.1;
-            return Math.max(Math.abs(value) - 0.1, 0) / 0.9 * Math.abs(value) / value;
+            value = Math.max(Math.abs(value) - 0.1, 0) / 0.9 * Math.abs(value) / value;
+            
+            if(Math.abs(value - oldValue) < 0.05 && value !== 1 && value !== -1 && value !== 0) {
+                // eh, value didn't change much and isn't at the limit - don't bother telling the server about it
+                return oldValue;
+            }
+            return value;
         }
 
         $interval(function() {
             var gp = navigator.getGamepads()[0];
             if (gp && gp.connected) {
-                self.drive = applyDeadzone(gp.axes[0]);
-                self.turn = applyDeadzone(gp.axes[1]);
+                self.turn = applyDeadzone(gp.axes[0], self.turn);
+                self.drive = -applyDeadzone(gp.axes[1], -self.drive);
             } else {
                 if (self.drive || self.turn) {
                     self.drive = 0;
