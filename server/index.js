cd228b0ab5f6c8b51f4763912983005ba7e61abb
jrupp <jrupp@rightpoint.com>
2016-03-13 22:08:19 -0500

    Get things working with new code

--- server/index.js
+++ server/index.js
@@ -19,6 +19,7 @@ io.on('connection', function(socket) {
         drive = Math.floor(Math.abs(data.turn) * 500);
         turn = data.turn > 0 ? -1 : 1; 
     }
+    // var send = '137 ' + (drive >> 8) + ' ' + (drive % 256) + ' ' + (turn >> 8) + ' ' + (turn  % 256);
     var send = new Buffer([ 137, drive >> 8, drive, turn >> 8, turn ]);
     console.log(send);
     serialClient.write(send);
@@ -44,37 +45,46 @@ serialClient.on('error', function(err) {
     console.log('err serial', err);
 });
 serialClient.connect(serialPort, host, function () {
-    console.log('connected serial, waiting for BRC wakup to finish');
-    setTimeout(function() {
-        // 16 nulls to clear out any pending commands,then a reset
-        serialClient.write(new Buffer([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7]));
-        console.log('wrote reset');
-        setTimeout(function() {
-            serialClient.write(new Buffer([128]));
-            console.log('wrote start');
-            setTimeout(function() {
-                serialClient.write(new Buffer([149, 35]));
-                console.log('requested status');
-                setTimeout(function() {
-                    serialClient.write(new Buffer([131]));
-                    console.log('wrote safe');
-                //     setTimeout(function() {
-                //         serialClient.write(new Buffer([137, 1, 0, 0, 0]));
-                //         console.log('wrote drive');
-                //         setTimeout(function() {
-                //             serialClient.write(new Buffer([137, 0, 0, 0, 0]));
-                //             console.log('wrote no drive');
-                //             // setTimeout(function() {
-                //             //     serialClient.write(new Buffer([173]));
-                //             //     console.log('wrote stop');
-                //             //     
-                //             // }, 500);
-                //         }, 500);
-                //     }, 100);
-                }, 100);
-            }, 2000);
-        }, 10000);
-    }, 2000);
+    console.log('connected serial');
+    
+    serialClient.write(new Buffer([149, 1, 35]));
+    console.log('requested status');
+    setInterval(function() {
+        serialClient.write(new Buffer([149, 1, 35]));
+        console.log('requested status');
+    }, 7000);
+    
+    // console.log('waiting for BRC wakeup to finish');
+    // setTimeout(function() {
+        // // 16 nulls to clear out any pending commands,then a reset
+        // serialClient.write(new Buffer([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7]));
+        // console.log('wrote reset');
+        // setTimeout(function() {
+        //     serialClient.write(new Buffer([128]));
+        //     console.log('wrote start');
+        //     setTimeout(function() {
+                // serialClient.write(new Buffer([149, 35]));
+                // console.log('requested status');
+                // setTimeout(function() {
+                //     serialClient.write(new Buffer([131]));
+                //     console.log('wrote safe');
+                // //     setTimeout(function() {
+                // //         serialClient.write(new Buffer([137, 1, 0, 0, 0]));
+                // //         console.log('wrote drive');
+                // //         setTimeout(function() {
+                // //             serialClient.write(new Buffer([137, 0, 0, 0, 0]));
+                // //             console.log('wrote no drive');
+                // //             // setTimeout(function() {
+                // //             //     serialClient.write(new Buffer([173]));
+                // //             //     console.log('wrote stop');
+                // //             //     
+                // //             // }, 500);
+                // //         }, 500);
+                // //     }, 100);
+                // }, 100);
+    //         }, 2000);
+    //     }, 10000);
+    // }, 2000);
 });
 serialClient.on('data', function (data) {
     console.log('got data: ', new Buffer(data));
@@ -90,15 +100,15 @@ controlClient.on('error', function(err) {
 });
 controlClient.connect(controlPort, host, function () {
     console.log('connected cmd');
-    controlClient.write(new Buffer([0,0,1,0,2,0]));
-    console.log('wrote startup');
-    setTimeout(function() {
-        controlClient.write(new Buffer([2,1]));
-        console.log('pulled BRC back up');
-    }, 1000);
+    // controlClient.write(new Buffer([0,0,1,0,2,0]));
+    // console.log('wrote startup');
+    // setTimeout(function() {
+    //     controlClient.write(new Buffer([2,1]));
+    //     console.log('pulled BRC back up');
+    // }, 1000);
 });
 controlClient.on('data', function (data) {
-    console.log('got data cmd: ' + data);
+    console.log('got data cmd: ' + new Buffer(data));
 });
 controlClient.on('close', function () {
     console.log('disconnected cmd');
