ae0c0051cc85f40db43f121966d00df6bed1ccbf
jrupp <jrupp@rightpoint.com>
2016-03-13 22:08:38 -0500

    Add smoothing to movement

--- web/src/home/homeController.js
+++ web/src/home/homeController.js
@@ -10,28 +10,43 @@
             if (!value) {
                 return value;
             }
-            var dz = 0.1;
-            value = Math.max(Math.abs(value) - 0.1, 0) / 0.9 * Math.abs(value) / value;
+            var dz = 0.3;
+            value = Math.max(Math.abs(value) - dz, 0) / (1-dz) * Math.abs(value) / value;
             
             if(Math.abs(value - oldValue) < 0.05 && value !== 1 && value !== -1 && value !== 0) {
                 // eh, value didn't change much and isn't at the limit - don't bother telling the server about it
+                console.log(value, oldValue);
                 return oldValue;
             }
             return value;
         }
+        
+        var interval = 25;
+        var maxDeltaPerSec = 2;
+        var maxDeltaPerInterval = maxDeltaPerSec / 1000 * interval; 
+        function applySmooth(value, oldValue) {
+            if(value == oldValue) {
+                return oldValue;
+            }
+            var delta = Math.min(Math.max(value - oldValue, -maxDeltaPerInterval), maxDeltaPerInterval);
+            return oldValue + delta;
+        }
 
+        var targetTurn = 0, targetDrive = 0;
         $interval(function() {
             var gp = navigator.getGamepads()[0];
             if (gp && gp.connected) {
-                self.turn = applyDeadzone(gp.axes[0], self.turn);
-                self.drive = -applyDeadzone(gp.axes[1], -self.drive);
+                targetTurn = applyDeadzone(gp.axes[0], targetTurn);
+                targetDrive = -applyDeadzone(gp.axes[1], -targetDrive);
+                self.turn = applySmooth(targetTurn, self.turn);
+                self.drive = applySmooth(targetDrive, self.drive);
             } else {
                 if (self.drive || self.turn) {
                     self.drive = 0;
                     self.turn = 0;
                 }
             }
-        }, 100);
+        }, 25);
 
         $scope.$watchCollection('[c.drive, c.turn]', function() {
             eventService.send('drive', { drive: self.drive, turn: self.turn });
